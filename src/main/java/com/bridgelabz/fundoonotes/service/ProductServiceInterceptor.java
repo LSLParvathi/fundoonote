a1b3fa6bd075ed7fbc98643076f8d88b98dddb31
LSLParvathi <mangalagirileela1997@gmail.com>
2020-03-24 10:43:41 +0530

    changes

--- src/main/java/com/bridgelabz/fundoonotes/service/ProductServiceInterceptor.java
+++ src/main/java/com/bridgelabz/fundoonotes/service/ProductServiceInterceptor.java
@@ -1,32 +1,41 @@
 package com.bridgelabz.fundoonotes.service;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse; 
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class ProductServiceInterceptor implements HandlerInterceptor {
-	
+
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(UserServiceImp.class);
 
-   @Override
-   public boolean preHandle
-      (HttpServletRequest request, HttpServletResponse response, Object handler) 
-      throws Exception {
-		log.info("Pre Handle method is Calling"); 
-      return true;
-   }
-   @Override
-   public void postHandle(HttpServletRequest request, HttpServletResponse response, 
-      Object handler, ModelAndView modelAndView) throws Exception {
-      log.info("Post Handle method is Calling"); 
-   }
-   @Override
-   public void afterCompletion
-      (HttpServletRequest request, HttpServletResponse response, Object 
-      handler, Exception exception) throws Exception {
-      log.info("Request and Response is completed"); 
-   }
-} 
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		log.info("Pre Handle method is Calling");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+		log.info("Post Handle method is Calling");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception exception) throws Exception {
+
+		String token = request.getHeader("token");
+		if (token == null) {
+			throw new RuntimeException("Authorization token is required");
+		}
+
+		log.info("Request and Response is completed");
+	}
+
+}
